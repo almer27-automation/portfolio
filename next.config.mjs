3c948de19248ff8680c59500780e9015138838d4
Almer Delos Reyes <almer.delos.reyes.dms@gmail.com>
2025-07-08 21:29:43 +0800

    Test fix

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,11 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+
+  output: 'export',
+  basePath: '/portfolio', // Replace with your repository name if deploying to a subpath
+  assetPrefix: '/portfolio/', // Ensure assets are loaded correctly
+  
 }
 
 export default nextConfig
